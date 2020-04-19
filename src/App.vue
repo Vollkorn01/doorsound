764e6622ca265d40a2670932ecddbfabb0d09a25
Alexander Pfyffer <alexander.pfyffer@gmail.com>
2020-04-19 13:35:14 +0200

    beautifying

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="moan logo" src="https://scontent.fzrh2-1.fna.fbcdn.net/v/t1.0-9/16602595_365534763821147_2942264932856879952_n.jpg?_nc_cat=105&_nc_sid=85a577&_nc_oc=AQl7PYqazHCXY9nis2ckv9GhFjA60BJwUx0B1VYugH0vGEgzSjV-4pKTvHkEdUesoHo&_nc_ht=scontent.fzrh2-1.fna&oh=88b98472d9c5291b5c6fb0f00f4fe144&oe=5EC24B8B" width="250" height= "250">
+    <HelloWorld msg="DOOR MOAN 1.0"/>
   </div>
 </template>
 
